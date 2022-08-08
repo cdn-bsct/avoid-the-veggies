2455adade1b756c42ede12a224e7e02a73e0bb68
cdn-bsct <justinarfeldman@gmail.com>
2022-08-08 15:05:06 -0400

    successfully rendered random veggie locations

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,9 @@
 /*----------- Constant Variables ---------------*/
-const totalSquares = 100;
-/*----------- State Variables ------------------*/
 
+/*----------- State Variables ------------------*/
+let vegBoard = [];
+let redundant = [];
+let randomNum;
 /*----------- Cached Elements ------------------*/
 gameBoard = document.getElementById('game-board')
 resetBtn = document.getElementById('reset').addEventListener('click', reset)
@@ -19,8 +21,25 @@ function reset() {
 function makeBoard() {
     console.log('i got the div board')
     for (let i = 0; i < 100; i++) {
-        let squ= document.createElement('div')
+        squ = document.createElement('div')
         squ.classList = "square"
+        squ.id = i
+        vegBoard.push(squ.id)
         gameBoard.append(squ)
     }
+    
+    for (i = 0; i < 30; i++) {
+        randomNum = Math.floor(Math.random() * 100)
+        vegBoard.forEach(function(el){
+            if (el == randomNum) {
+                redundant.push(el)
+                redundant.forEach( function (re) {
+                    if (el !== re) {
+                        let matchSquare = document.getElementById(el)
+                        matchSquare.innerHTML = "V"
+                    } 
+                })               
+            }
+        })
+        }
 }
